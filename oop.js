222ac61c085d09c482845d16de3d4ae60ee79d87
Eguono <efeguono@gmail.com>
2017-04-10 09:02:03 +0100

    Added an instance of the Laptop Object that depicts polymorphism

--- oop.js
+++ oop.js
@@ -29,3 +29,16 @@ class Laptop {
         return this.brand + " " + this.model + " is starting up !!!";
     }
 }
+
+//Demonstrates Principle of Inheritance**
+class Dell extends Laptop {
+    constructor(brand, model, oS) {
+        // The reserved 'super' keyword is for making super-constructor
+        super(brand, model, oS);
+    }
+
+    //Example Demonstrates Polymorphism
+    getBrandMotto() {
+        return this.brand + ", Purely You";
+    }
+}
