0c5d016c2e225ed781b4bf718ab165b679820b95
Eguono <efeguono@gmail.com>
2017-04-10 08:53:30 +0100

    Created a laptop class

---
+++ oop.js
@@ -0,0 +1,13 @@
+'use strict';
+
+class Laptop {
+
+    // custom class constructor
+    constructor(brand, model, oS, color) {
+        this.brand = brand; //Brand of Laptop
+        this.model = model; //Model of Brand
+        this.operatingSystem = oS; //Operating System of Laptop
+        this.productionDate = new Date();
+    }
+
+}
